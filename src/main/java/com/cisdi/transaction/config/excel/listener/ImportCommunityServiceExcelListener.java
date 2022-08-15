0163303ef4c7c1ae4d809a6f77fba9393d62b421
tgl <tgldai@qq.com>
2022-08-15 10:58:42 +0800

    导入不同模板提示成功修复

--- src/main/java/com/cisdi/transaction/config/excel/listener/ImportCommunityServiceExcelListener.java
+++ src/main/java/com/cisdi/transaction/config/excel/listener/ImportCommunityServiceExcelListener.java
@@ -3,14 +3,17 @@ package com.cisdi.transaction.config.excel.listener;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.exception.ExcelAnalysisException;
+import com.alibaba.excel.metadata.CellData;
 import com.cisdi.transaction.config.excel.ExcelImportValid;
 import com.cisdi.transaction.config.excel.ExceptionCustom;
 import com.cisdi.transaction.domain.dto.CommunityServiceDTO;
 import com.cisdi.transaction.service.MechanismInfoService;
+import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author yuw
@@ -34,6 +37,24 @@ public class ImportCommunityServiceExcelListener extends AnalysisEventListener<C
 
     public ImportCommunityServiceExcelListener(MechanismInfoService mechanismInfoService) {
         this.mechanismInfoService = mechanismInfoService;
+    }
+
+    private List<String> columns= Arrays.asList(
+            "13-2开办有偿社会中介和法律服务机构或者从业的情况",
+            "干部基本信息",
+            "有关事项信息",
+            "干部姓名","身份证号","工作单位","现任职务","职务层次","标签","职级","人员类别","政治面貌","在职状态","姓名","称谓","执业资格名称","执业证号","开办或所在机构名称","统一社会信用代码/注册号","经营（服务）范围","成立日期","注册地","城市","经营（服务）地","机构类型","注册资本（金）或资金数额（出资额）（人民币万元）","经营状态","是否为机构股东（合伙人、所有人等）","个人认缴出资额或个人出资额（人民币万元）","个人认缴出资比例或个人出资比例（%）","入股（合伙）时间","是否在该机构中从业","所担任的职务名称","入职时间","是否与报告人所在单位（系统）直接发生过经济关系","备注","填报类型","年度","有无此类情况"
+    );
+
+    @Override
+    public void invokeHead(Map<Integer, CellData> var1, AnalysisContext var2){
+        if (!CollectionUtils.isEmpty(var1)){
+            for (CellData value : var1.values()) {
+                if (Objects.nonNull(value)&& StringUtils.isNotBlank(value.toString())&&!columns.contains(value.toString())){
+                    throw new ExcelAnalysisException("模板不包含此字段");
+                }
+            }
+        }
     }
 
     @Override
