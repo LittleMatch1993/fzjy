1b6996d28a20882286b6631ce02c4652e88c167a
me <1033586365@qq.com>
2022-08-15 14:17:10 +0800

    1111

---
+++ src/main/java/com/cisdi/transaction/domain/vo/OrgDictVo.java
@@ -0,0 +1,14 @@
+package com.cisdi.transaction.domain.vo;
+
+import lombok.Data;
+
+/**
+ * @Author: cxh
+ * @Description:
+ * @Date: 2022/8/14 23:40
+ */
+@Data
+public class OrgDictVo {
+    String id;
+    String name;
+}
