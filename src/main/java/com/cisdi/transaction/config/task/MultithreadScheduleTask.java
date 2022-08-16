7b3690d95d32ccf0e35ff17cf4eda6bf6513cdd8
me <1033586365@qq.com>
2022-08-16 19:58:34 +0800

    修改定时任务时间

--- src/main/java/com/cisdi/transaction/config/task/MultithreadScheduleTask.java
+++ src/main/java/com/cisdi/transaction/config/task/MultithreadScheduleTask.java
@@ -28,7 +28,7 @@ public class MultithreadScheduleTask {
     private GbBasicInfoService gbBasicInfoService;
 
     //@PostConstruct      //项目启动执行1次
-    @Scheduled(fixedDelay = 72000000) //两小时执行一次
+    @Scheduled(fixedDelay = 7200000) //两小时执行一次
     public  void syncOrgInfo(){
         long i = DateUtil.date().getTime();
         System.out.println("执行组织同步定时任务");
@@ -38,7 +38,7 @@ public class MultithreadScheduleTask {
     }
 
    // @PostConstruct      //项目启动执行一次
-    @Scheduled(fixedDelay = 72000000) //两小时执行一次
+    @Scheduled(fixedDelay = 7200000) //两小时执行一次
     public  void syncGbBasicInfo(){
         long i = DateUtil.date().getTime();
         System.out.println("执行干部信息定时任务");
