5148ec73efc05c708d4760c71c67ea969110af37
me <1033586365@qq.com>
2022-08-29 22:14:55 +0800

    1.职位为null时，禁止交易表显示bug,2.禁止交易记录表创建时间

--- src/main/java/com/cisdi/transaction/service/impl/MechanismInfoServiceImpl.java
+++ src/main/java/com/cisdi/transaction/service/impl/MechanismInfoServiceImpl.java
@@ -176,7 +176,7 @@ public class MechanismInfoServiceImpl extends ServiceImpl<MechanismInfoMapper, M
             }
             if(CollectionUtil.isEmpty(gbOrgList)){
                 //this.updateState(ids, SystemConstant.SAVE_STATE);
-                return ResultMsgUtil.failure("没有找到干部组织信息");
+                return ResultMsgUtil.failure("没有找到干部组织信息",ids);
             }
            /* if(CollectionUtil.isNotEmpty(tempList)){
                 infoList = infoList.stream().filter(e->tempList.contains(e.getId())).collect(Collectors.toList());
